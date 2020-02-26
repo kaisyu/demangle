e8cfeb14dc2519a02d76c42d241b52feecdf9b58
sanggi0.lee <sanggi0.lee@lge.com>
2020-02-26 12:02:46 +0900

    add nonewline option

--- src/demangle.cc
+++ src/demangle.cc
@@ -28,15 +28,26 @@ static const char personality[] = "Microsoft Visual Studio";
 static const char personality[] = "Unknown";
 #endif
 
-static void demangle(const char *mangled_name, bool quiet) {
+static void demangle(const char *mangled_name, bool quiet, bool nonewline) {
   int status = 0;
   const char *realname = abi::__cxa_demangle(mangled_name, 0, 0, &status);
+  const char *fmtstr = NULL;
   switch (status) {
   case 0:
     if (quiet) {
-      puts(realname);
+      if (nonewline) {
+        fmtstr ="%s";
+      } else {
+        fmtstr = "%s\n";
+      }
+      printf(fmtstr, realname);
     } else {
-      printf("%s  %s\n", realname, mangled_name);
+      if (nonewline) {
+        fmtstr = "%s %s";
+      } else {
+        fmtstr = "%s %s\n";
+      }
+      printf(fmtstr, realname, mangled_name);
     }
     break;
   case -1:
@@ -56,22 +67,27 @@ static void demangle(const char *mangled_name, bool quiet) {
 
 int main(int argc, char **argv) {
   int c;
+  int cnt = 0;
   bool quiet = false;
+  bool nonewline = false;
   for (;;) {
     static struct option long_options[] = {{"help", no_argument, 0, 'h'},
+                                           {"nonewlone", no_argument, 0, 'n'},
                                            {"personality", no_argument, 0, 'p'},
                                            {"quiet", no_argument, 0, 'q'},
                                            {0, 0, 0, 0}};
     int option_index = 0;
-    c = getopt_long(argc, argv, "hpq", long_options, &option_index);
+    c = getopt_long(argc, argv, "hnpq", long_options, &option_index);
     if (c == -1) {
       break;
     }
     switch (c) {
     case 'h':
-      printf("Usage: %s [-h|--help] [-p|--personality] [-q|--quiet] "
+      printf("Usage: %s [-h|--help] [-n|--nonewline] [-p|--personality] [-q|--quiet] "
              "[MANGLED]...\n", argv[0]);
       return 0;
+    case 'n':
+      nonewline = true;
     case 'p':
       printf("Personality: %s\n", personality);
       break;
@@ -86,6 +102,10 @@ int main(int argc, char **argv) {
   }
 
   for (int i = optind; i < argc; i++) {
-    demangle(argv[i], quiet);
+    if (nonewline && cnt > 0) {
+      printf(" ");
+    }
+    demangle(argv[i], quiet, nonewline);
+    cnt++;
   }
 }
